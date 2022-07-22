ab09b5b963715ef55abe05eca509d273fec3ea8e
yuvanis <uvnistratov@gmail.com>
2022-07-22 15:28:02 +0300

    feat: add code to numbers-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,9 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  if (arr.includes(value)) return arr.indexOf(value);
+  else return -1;
 }
 
 /**
